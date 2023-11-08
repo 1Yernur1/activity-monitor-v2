88831e031aa2788c4ea72e28e5d92dbd07918f72
Yernur <shalgymbayev.yernur2001@gmail.com>
2023-11-08 10:56:59 +0600

    V2-11 made error text color red

--- components/FormErrorText.tsx
+++ components/FormErrorText.tsx
@@ -2,7 +2,7 @@ import FormHelperText from "@mui/material/FormHelperText/FormHelperText";
 
 export const FormErrorText = ({ errorText }: { errorText: string }) => {
   return (
-    <FormHelperText className="text-red-500 text-center text-base">
+    <FormHelperText component="p" className="text-red-500 text-center text-base">
       {errorText}
     </FormHelperText>
   );
