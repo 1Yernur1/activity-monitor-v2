1b8a93ad97418f1404b084a1bd4dd7dd9bb87a78
Yernur <shalgymbayev.yernur2001@gmail.com>
2023-11-11 22:35:21 +0600

    V2-25 resolve bug with loading spinner

--- components/GlobalLoadingSpinner.tsx
+++ components/GlobalLoadingSpinner.tsx
@@ -2,7 +2,7 @@ import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
 
 export const GlobalLoadingSpinner = () => {
   return (
-    <div className=" h-96 w-full grid place-items-center col-span-full">
+    <div className=" h-96 w-full grid place-items-center col-span-full col-start-2">
       <CircularProgress />
     </div>
   );
